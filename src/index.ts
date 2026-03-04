a595db21f8603edff049a77170437765588c2d91
alsaeedfayed <alsaeedfayed@gmail.com>
2026-03-04 13:40:57 -0500

    feat: :fire: init tailwind & shadcn & build & export button component

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Utilities
+export { cn } from "./lib/utils";
+
+// Components
+export { Button, buttonVariants } from "../components/ui/button";
+export type { ButtonProps } from "../components/ui/button";
