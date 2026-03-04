304bc2727c6ac7cf6edc714b42e804841c008491
alsaeedfayed <alsaeedfayed@gmail.com>
2026-03-04 14:17:54 -0500

    feat: :fire: progress

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { cn } from "./lib/utils";
 // Components
 export { Button, buttonVariants } from "../components/ui/button";
 export type { ButtonProps } from "../components/ui/button";
+
+export { Progress } from "../components/ui/progress";
